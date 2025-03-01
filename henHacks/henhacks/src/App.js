a236165084923af491bc0ca02b24dfd2174b8156
JoshuCW <joshwash@udel.edu>
2025-03-01 16:03:29 -0500

    stuff

--- henHacks/henhacks/src/App.js
+++ henHacks/henhacks/src/App.js
@@ -3,15 +3,16 @@ import './index.css';
 
 
 function App() {
-  var message = "Info here";
+  // eslint-disable-next-line no-useless-concat
+  var message = "Extra info will display here if applicable \n upon press of the analysis button";
   return (
     <div className="App">
-      <header className=' = App'>
+      <header>
         <p>
-          Additional Info(per req)
+          Additional Info
         </p>
         <p>
-          <text>
+          <text className="App-txt" style={{backgroundColor: '##332c2c', color: 'white', height: 10}}>
             {message}
           </text>
         </p>
